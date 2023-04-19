249acfcc5dfcc413401ec849ea7c50535057d73d
R.Quentin <59691299+Marshe78@users.noreply.github.com>
2023-04-19 17:15:36 +0200

    test

--- server/index.js
+++ server/index.js
@@ -11,3 +11,4 @@ io.on('connection', socketManage )
 // In dev mode just hide hide app.uss(... ) below
 app.use( express.static(path.join(__dirname, '../build')))
 server.listen( PORT, () => console.log('App was start at port : ' + PORT ))
+//test
